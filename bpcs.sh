5cb47096a417990735885e2b749cef6efc63d66f
左契 <guofa.tgf@alipay.com>
2016-01-29 20:13:40 +0800

    enable retry

--- bpcs.sh
+++ bpcs.sh
@@ -5,7 +5,6 @@ FOLDER="/private/etc"
 STRIP="/private/"
 # -----------------
 
-set -e
 cd $(dirname "$0")
 
 if [[ "$1" = init ]]; then
@@ -41,7 +40,7 @@ echo '#' $(date '+%F %T') "start $today ----------" >> $log_file
 
 if [[ -e $today ]]; then
 	touch $today_done
-	resume=$(wc -l $today_done | awk "{print \$0+1}")
+	resume=$(wc -l $today_done | awk '{print $0+1}')
 else
 	rm -f $today_done
 	sentry="_sentry"
@@ -55,50 +54,55 @@ else
 	mv -f $sentry_new $sentry
 	resume=1
 fi
+
 limit=${1-0}
 batch=${2-200}
-echo '-' $(date '+%F %T') "resume=$resume limit=$limit batch=$batch" >> $log_file
+retry=${3-10}
 
-
-counter=$(awk -v url="https://pcs.baidu.com/rest/2.0/pcs/file?method=upload&access_token=${atoken}&ondup=overwrite&path=/apps/${appname}" '
-function upload(done) {
-	if (cc > 0) {
-		ret = system("curl " substr(args, 5) " 2>_err >_out")
-		if (ret == 0) {
-			system("cat _out >> '$today_done'")
-			cc = 0
-			args = ""
-		} else {
-			"sed -n /^{/d _out | wc -l" | getline v
-			if (v + 0) system("sed -n /^{/d _out >> '$today_done'")
-			print counter - cc + v
+while [[ $retry -ge 0 ]]; do
+	echo '-' $(date '+%F %T') "resume=$resume limit=$limit batch=$batch retry=$retry" >> $log_file
+	counter=$(awk -v url="https://pcs.baidu.com/rest/2.0/pcs/file?method=upload&access_token=${atoken}&ondup=overwrite&path=/apps/${appname}" '
+	function upload(done) {
+		if (cc > 0) {
+			ret = system("curl " substr(args, 5) " 2>_err >_out")
+			if (ret == 0) {
+				system("cat _out >> '$today_done'")
+				cc = 0
+				args = ""
+			} else {
+				"sed -n /^{/d _out | wc -l" | getline v
+				if (v + 0) system("sed -n /^{/d _out >> '$today_done'")
+				print counter - cc + v
+				ended = 1
+				exit ret
+			}
+		}
+		if (done != "batch") {
+			print counter + 0
 			ended = 1
-			exit ret
+			exit 0
 		}
 	}
-	if (done != "batch") {
-		print counter + 0
-		ended = 1
-		exit 0
+	(NR >= '$resume') {
+		args = args " -: -kfLsS -X POST --form file=@\"" $0 "\" -w \" %{http_code} %{time_total}\\n\" \"" url substr($0, '${#STRIP}') "\""
+		counter += 1
+		cc += 1
+		if (cc == '$batch') upload("batch")
+		if (counter == '$limit') upload("limit")
 	}
-}
-(NR >= '$resume') {
-	args = args " -: -kfLsS -X POST --form file=@\"" $0 "\" -w \" %{http_code} %{time_total}\\n\" \"" url substr($0, '${#STRIP}') "\""
-	counter += 1
-	cc += 1
-	if (cc == '$batch') upload("batch")
-	if (counter == '$limit') upload("limit")
-}
-END {
-	if (!ended) upload("end")
-}' $today)
-
-ret=$?
-if [[ $ret -eq 0 ]]; then
-	echo '-' $(date '+%F %T') "complete $counter" >> $log_file
-else
-	err=$(cat _err)
-	echo '-' $(date '+%F %T') "abort $counter $err" >> $log_file
-fi
+	END {
+		if (!ended) upload("end")
+	}' $today)
+	ret=$?
+	if [[ $ret -eq 0 ]]; then
+		echo '-' $(date '+%F %T') "complete $counter" >> $log_file
+		break
+	else
+		err=$(cat _err)
+		echo '-' $(date '+%F %T') "abort $counter $err" >> $log_file
+		resume=$(wc -l $today_done | awk '{print $0+1}')
+		let retry--
+	fi
+done
 
 exit $ret
